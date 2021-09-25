4027391428445a04696817be1f6b0f1e8425a9cb
Colin <c.djerdak@gmail.com>
2021-09-25 22:26:04 +0200

    new styling

---
+++ index.js
@@ -0,0 +1,16 @@
+function html(){
+    window.location = "/html-area.html";
+}
+
+function css(){
+    window.location = "/css-area.html";
+}
+
+function php(){
+    window.location = "/php-area.html";
+}
+
+function js(){
+    window.location = "/js-area.html";
+}
+
