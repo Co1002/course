61baf54634b08468dc549037c7a4ae93af0c09ff
Colin <c.djerdak@gmail.com>
2021-10-02 20:00:04 +0200

    security

---
+++ pw.js
@@ -0,0 +1,11 @@
+var password;
+
+var pass1="cool";
+
+password=prompt('Bitte Passwort eingeben:',' ');
+
+if (password==pass1){
+    alert('Password Correct!');
+}else{
+    window.location="/";
+}
