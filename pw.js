2e9a12eb807b63a16f18824b1d86edcae57f1adb
Colin <c.djerdak@gmail.com>
2021-10-02 20:37:38 +0200

    Update pw.js

--- pw.js
+++ pw.js
@@ -1,10 +1,36 @@
-var password;
+const passwords = ['KARpf'];
 
-var pass1="KARpf";
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-password=prompt('Bitte Passwort eingeben:',' ');
-
-if (password!==pass1){
-    alert("Passwort Falsch!");
-    window.location="/";
+let pwInput = getCookie("pwInput");
+if (pwInput == "") {
+    var password;
+    password=prompt('Bitte Passwort eingeben:',' ');
+    if(passwords.includes(password)){
+        setCookie("pwInput", pwEntered, 30);
+    }else{
+        alert("Passwort Falsch!");
+        window.location="/";
+    }
 }
