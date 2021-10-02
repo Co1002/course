909c929f21b44054fe221bc2a8f4f10072cdb091
Colin <c.djerdak@gmail.com>
2021-10-02 20:17:07 +0200

    Update pw.js

--- pw.js
+++ pw.js
@@ -1,9 +1,10 @@
 var password;
 
-var pass1="cool";
+var pass1="KARpf";
 
 password=prompt('Bitte Passwort eingeben:',' ');
 
 if (password!==pass1){
+    alert("Passwort Falsch!");
     window.location="/";
 }
